48c98db3f1bcb0e888cd2b28958954d0945be914
zNotChill <59340653+zNotChill@users.noreply.github.com>
2022-02-04 23:56:03 +0000

    Update serverinfo.js

--- aspiired.cf/cubedapi/serverinfo.js
+++ aspiired.cf/cubedapi/serverinfo.js
@@ -1,10 +1,19 @@
-fetch(`https://thingproxy.freeboard.io/fetch/https://api.playerservers.com/servers`).then(res => res.json())
+fetch("https://thingproxy.freeboard.io/fetch/https://api.playerservers.com/servers", {
+	method: 'POST', // *GET, POST, PUT, DELETE, etc.
+	mode: 'cors', // no-cors, *cors, same-origin
+	cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+	credentials: 'same-origin', // include, *same-origin, omit
+	headers: {
+		'Content-Type': 'application/json'
+		// 'Content-Type': 'application/x-www-form-urlencoded',
+	},
+	redirect: 'follow', // manual, *follow, error
+	referrerPolicy: 'no-referrer' // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+}).then(res => res.json())
 .then(json => {
-    let collection = []
     var servers = json.servers
     for (const server of servers) {
         if (server.name !== "lobby" && server.name !== "lobby2") {
-           
 						
 						var node = document.createElement('div')
 						node.insertAdjacentHTML("beforeend", `<div class="server">
